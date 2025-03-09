93e82116135814c2b7d447ff53af97b1fd543173
Minh Nghiem <mngh037@gmail.com>
2025-03-09 18:44:24 -0500

    Add write stream effects

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import json
 import altair as alt
 import polars as pl
 import plotly.graph_objects as go
+import time
 
 load_dotenv()
 MODEL = "gpt-4o"
@@ -48,15 +49,37 @@ def review_resume(resume, job_description):
         response_format={"type": "json_object"}
     )
 
+    # print(response.choices[0].message.content)
     result = json.loads(str(response.choices[0].message.content))
     return result
+
+# Streaming UI
+def stream_data(text, chart=None):
+    for word in text.split(" "):
+        yield word + " "
+        time.sleep(0.05)
+    
+    placeholder = st.empty()
+
+    if chart is not None:
+        placeholder.altair_chart(chart)
 
 # Set page config for streamlit UI
 st.set_page_config(
     page_title="Resume Review",
     page_icon=":memo:",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state="expanded"
+)
+
+# Widen default 46rem width of main block
+st.html("""
+    <style>
+        .stMainBlockContainer {
+            max-width:60rem;
+        }
+    </style>
+    """
 )
 
 # Initialize session state
@@ -97,13 +120,14 @@ with st.sidebar:
             st.session_state.uploaded = True
 
 st.title(":memo: Review My Resume")
+# Disappear when user clicks "review"
 if not st.session_state.uploaded:
     st.write("Intro and some responsible AI stuff goes here.")
 
 if st.session_state.uploaded:
-    # with open("./sample_result.json", "r") as f:
-    #     result = json.load(f)
-    result = review_resume(resume_text, jd_text)
+    with open("./sample_result.json", "r") as f:
+        result = json.load(f)
+    # result = review_resume(resume_text, jd_text)
 
     overall_score = result["overall_match"]
     def color(score):
@@ -113,24 +137,27 @@ if st.session_state.uploaded:
             return "#fea"
         return "#faa"
     
+    with st.spinner("Analyzing in progress..."):
+        time.sleep(4)
+    
     annotated_text("Your resume overall match score against this JD is ", (f"{overall_score}/100", "", color(overall_score)),)
-
+    
     tab1, tab2, tab3, tab4 = st.tabs(["Overview", "Key Skills Assessment", "Fit Evaluation", "Areas for Improvement"])
 
     with tab1:
-        st.write(result["general_feedback"])
-
         data = pl.DataFrame(result["score_breakdown"])
-        c = (alt.Chart(data).mark_bar().encode(
+        scores_chart = (alt.Chart(data).mark_bar().encode(
             y=alt.Y('category:N', axis=alt.Axis(labelLimit=300), title=''),
             x=alt.X('score:Q', axis=alt.Axis(values=list(range(0, 120, 10))), scale=alt.Scale(domain=[0, 100]))
         ).properties(
             width=800,
             height=300
         ))
-        st.altair_chart(c)
+
+        st.write_stream(stream_data(result["general_feedback"], chart=scores_chart))
 
     with tab2:
+        # Reference: https://plotly.com/python/radar-chart/
         skill = []
         score = []
         comment = []
@@ -139,49 +166,61 @@ if st.session_state.uploaded:
             score.append(item["score"])
             comment.append(item["comment"])
 
+        layout = go.Layout(
+            margin=go.layout.Margin(
+                l=0,
+                r=80
+            )
+        )
+
         fig = go.Figure(data=go.Scatterpolar(
             r=score,
             theta=skill,
             fill='toself',
             line_width=0,
             name=""
-            )
+            ), layout=layout
         )
+
         fig.update_layout(
             polar=dict(
                 radialaxis=dict(
                     visible=True,
-                    range=[ 0,5 ]
+                    range=[0, 5]
                 )
             )
         )
 
-        col1, col2 = st.columns([2, 2])
+        col1, col2 = st.columns([1.75, 1.25])
         col1.plotly_chart(fig)
-        for i in range(5):
-            col2.write(f"**{skill[i]}**: {comment[i]}")
+        
+        comment_container = col2.container(border=True)
+        with comment_container:
+            for i in range(5):
+                st.write_stream(stream_data(f"**{skill[i]}**: {comment[i]}"))
 
     with tab3:
-        st.write(result["fit"]["fit_evaluation"])
-        st.write(f"Strengths:")
+        st.write_stream(stream_data(result["fit"]["fit_evaluation"]))
+        st.write_stream(stream_data(f"Strengths:"))
         for i, strength in enumerate(result["fit"]["strengths"]):
-            st.write(f"{i+1}. {strength}")
+            st.write_stream(stream_data(f"{i+1}. {strength}"))
 
     with tab4:
         st.subheader("Resume")
-        st.write("Some weak bullet points you can improve on.")
+        st.write_stream(stream_data("Some weak bullet points you can improve on."))
         for i, d in enumerate(result["resume_content"]):
+            time.sleep(0.1)
             annotated_text((f"{i+1}. {d['weak_bullet_point']}", ''),)
             for key, val in d["suggestion"].items():
-                st.write(f"• **{key}**: {val}")
-            st.write(f"**=> Revised Example**: {d['revised_example']}")
+                st.write_stream(stream_data(f"• **{key}**: {val}"))
+            st.write_stream(stream_data(f"**=> Revised Example**: {d['revised_example']}"))
 
         st.subheader("Missing skills/qualifications")
         for item in result["missing_qualifications"]:
-            st.write(f":x: {item}")
+            st.write_stream(stream_data(f":x: {item}"))
 
         st.subheader("Industry Enhancements")
-        st.write(result["profile_enhancements"]["industry_trend"])
-        st.write(result["profile_enhancements"]["career_advice"])
+        st.write_stream(stream_data(result["profile_enhancements"]["industry_trend"]))
+        st.write_stream(stream_data(result["profile_enhancements"]["career_advice"]))
         for item in result["profile_enhancements"]["action_steps"]:
-            st.write(f":key: {item}")
+            st.write_stream(stream_data(f":key: {item}"))
